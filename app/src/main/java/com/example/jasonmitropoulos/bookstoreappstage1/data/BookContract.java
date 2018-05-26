3eb2cd147a0189ad72a54400e943272f609b9b6b
JasonJMitro <jasonjmitro@gmail.com>
2018-05-26 14:40:07 +0100

    changes to BookContract

--- app/src/main/java/com/example/jasonmitropoulos/bookstoreappstage1/data/BookContract.java
+++ app/src/main/java/com/example/jasonmitropoulos/bookstoreappstage1/data/BookContract.java
@@ -11,13 +11,13 @@ public class BookContract {
 
         public final static String _ID = BaseColumns._ID;
 
-        public final static String COLUMN_PRODUCT_NAME ="product name";
+        public final static String COLUMN_PRODUCT_NAME ="product_name";
 
         public final static String COLUMN_PRICE = "price";
 
         public final static String COLUMN_QUANTITY = "quantity";
 
-        public final static String COLUMN_SUPPLIER_NAME = "supplier name";
+        public final static String COLUMN_SUPPLIER_NAME = "supplier_name";
 
         public final static String COLUMN_SUPPLIER_PHONE = "phone";
 
